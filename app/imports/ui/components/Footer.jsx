c31cab9b548c4596bb93c50e84b79c2ed4b3bb99
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2019-11-19 10:08:34 -1000

    Updated Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -6,14 +6,17 @@ class Footer extends React.Component {
     const divStyle = { paddingTop: '15px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-          </div>
-        </footer>
+        <div style={divStyle} className="ui center aligned container">
+        <hr />
+        <p><strong>University of Hawaiʻi at Mānoa <script>document.write(new Date().getFullYear());</script></strong>
+    <address>
+      2465 Campus Road, Room 208, Honolulu, HI 96822
+    Phone:(808)956-8178 &nbsp;
+    Email: <a href="mailto:sld@hawaii.edu">sld@hawaii.edu</a>
+    </address>
+  </p>
+  </div>
+  </footer>
     );
   }
 }
