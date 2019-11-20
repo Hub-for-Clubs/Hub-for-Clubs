e532f30cfb694f677f02bf140697111043b95e54
RyutoKitagawa <ryu51920@gmail.com>
2019-11-19 14:36:29 -1000

    Fix Merge Conflict

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const divStyle = { paddingTop: '15px' };
     return (
         <footer className='footer'>
-          <div className="ui center aligned container">
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
+          <div style={divStyle} className="ui center aligned container">
+            <hr />
+            <p><strong>University of Hawaiʻi at Mānoa <script>document.write(new Date().getFullYear());</script></strong>
+              <address>
+                2465 Campus Road, Room 208, Honolulu, HI 96822
+                Phone:(808)956-8178 &nbsp;
+                Email: <a href="mailto:sld@hawaii.edu">sld@hawaii.edu</a>
+              </address>
+            </p>
           </div>
         </footer>
     );
