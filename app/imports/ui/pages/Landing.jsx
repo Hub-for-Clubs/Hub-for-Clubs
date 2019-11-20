f1b40f1327837bda2d4716f6b2bcc02221c23803
RyutoKitagawa <ryu51920@gmail.com>
2019-11-19 14:30:41 -1000

    Create Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import '../../../client/style.css';
+import { Grid, Form } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const gridStyle = { paddingTop: '30px' };
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular
-                   src="https://upload.wikimedia.org/wikipedia/en/3/35/University_of_Hawaii_seal.svg"/>
+        <div className="landing">
+        <Grid style={ gridStyle } verticalAlign='middle' textAlign='center' container>
+          <Grid.Column width={5}>
+            <Grid.Row>
+              <div className='description_title'>Finding Clubs That Are Right For You</div>
+            </Grid.Row>
+            <Grid.Row style={{ padding: '2em', textAlign: 'left' }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Grid.Row>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to Hub-for-Clubs</h1>
-          </Grid.Column>
+          <Grid.Column width={2}/>
 
+          <Grid.Column width={5}>
+            <Grid.Row>
+              <h1 className='hc_green massive'>Hub for Clubs</h1>
+            </Grid.Row>
+            <Grid.Row>
+              <Form className='sign_up_form'>
+                <Form.Field>
+                  <Form.Input fluid label='Username:' placeholder='Username' />
+                  <Form.Input fluid label='Email:' placeholder='Email'/>
+                  <Form.Input fluid label='Password:' placeholder='Password' type='password'/>
+                  <Form.Button fluid className='sign_up_button'>Sign Up</Form.Button>
+                </Form.Field>
+              </Form>
+            </Grid.Row>
+          </Grid.Column>
         </Grid>
+        </div>
     );
   }
 }
