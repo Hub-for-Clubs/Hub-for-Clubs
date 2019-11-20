8d0ece46987a87d89e1402763e0eb05bfe4101ae
RyutoKitagawa <ryu51920@gmail.com>
2019-11-19 17:42:42 -1000

    Create Sign In Functionality

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -7,14 +7,14 @@ class Footer extends React.Component {
     return (
         <footer className='footer'>
           <div style={divStyle} className="ui center aligned container">
-            <hr />
-            <p><strong>University of Hawaiʻi at Mānoa <script>document.write(new Date().getFullYear());</script></strong>
-              <address>
-                2465 Campus Road, Room 208, Honolulu, HI 96822
-                Phone:(808)956-8178 &nbsp;
-                Email: <a href="mailto:sld@hawaii.edu">sld@hawaii.edu</a>
-              </address>
+            <p><strong>University of Hawaiʻi at Mānoa
+              <script>document.write(new Date().getFullYear());</script></strong>
             </p>
+            <address>
+              2465 Campus Road, Room 208, Honolulu, HI 96822
+              Phone:(808)956-8178 &nbsp;
+              Email: <a href="mailto:sld@hawaii.edu">sld@hawaii.edu</a>
+            </address>
           </div>
         </footer>
     );
