363598a55e93175828eac070d3a2110b8513fd21
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2019-11-21 18:52:43 -1000

    basic for profile

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader, Grid } from 'semantic-ui-react';
+import { Container, Header, Loader, Grid } from 'semantic-ui-react';
 import { Stuffs } from '/imports/api/stuff/Stuff';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -18,30 +18,44 @@ class Profile extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center">Profile</Header>
-          <Grid>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Row>
-              <Table.Cell>
-                <img src='/images/HubForClubsLogo.png'></img>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Body>
-              <Table.Cell>
+          <Grid class="ui two column grid">
+          <div className="four wide column">
+              <div className="row">
+                <h1>Name</h1>
+              </div>
+            <div className="row">
+                <img src='/images/HubForClubsLogo.png' width='100px' height='100px'></img>
+            </div>
+              <div className="row">
                 Interests
-              </Table.Cell>
-              <Table.Cell>
+              </div>
+              <div className="row">
                 Bio
-              </Table.Cell>
-              <Table.Cell>
+              </div>
+              <div className="row">
                 Clubs
-              </Table.Cell>
-            </Table.Body>
-          </Table>
+              </div>
+          </div>
+          <div className="twelve wide column">
+            <div className="row">
+                <h1>Clubs Joined</h1>
+            </div>
+            <div className="row">
+              <h2>Clubs Joined</h2>
+            </div>
+            <div className="row">
+                <h1>Clubs Admin</h1>
+            </div>
+            <div className="row">
+              <h2>Clubs Admin</h2>
+            </div>
+            <div className="row">
+                <h1>Announcements</h1>
+            </div>
+            <div className="row">
+              <h2>Announcements</h2>
+            </div>
+          </div>
           </Grid>
         </Container>
     );
@@ -57,7 +71,7 @@ Profile.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  const subscription = Meteor.subscribe('Stuffs');
   return {
     stuffs: Stuffs.find({}).fetch(),
     ready: subscription.ready(),
